9e34039dabf82e07b8474d5bbd8098aedc41b162
ChristosKyriazis <101571234+ChristosKyriazis@users.noreply.github.com>
2022-06-11 04:08:19 -0400

    Code refactorings

--- Source/js/Editor.js
+++ Source/js/Editor.js
@@ -6,207 +6,212 @@ import Spices from './Spices.js'
 import Recipe from './Recipe.js';
 
 
-(() => {
-
-    const { log } = console;
+const { log } = console;
 
 
-    const menu = new Menu('Editor');
-
-    menu.onOpen = async (editor,recipe) => {
-
-        recipe ??= new Recipe;
+function ignore(){}
 
 
-        const label = create('input');
-        label.placeholder = 'Recipe Name';
-        label.type = 'text';
-        label.value = recipe.name;
-        label.addEventListener('input',onEditName);
-        editor.appendChild(label);
+const menu = new Menu('Editor');
 
-        const spicelists = create('div');
-        spicelists.classList.add('Spices');
-        editor.appendChild(spicelists);
+menu.onOpen = async (editor,recipe) => {
 
-        const used_label = create('div');
-        used_label.textContent = 'Used';
-        used_label.classList.add('Label');
-        used_label.title = 'Spices used in your recipe.';
-        spicelists.appendChild(used_label);
-
-        spicelists.appendChild(create('div'));
-
-        const unused_label = create('div');
-        unused_label.textContent = 'Available';
-        unused_label.classList.add('Label');
-        unused_label.title = 'Spices you can add to your recipe.';
-        spicelists.appendChild(unused_label);
-
-        const used = create('div');
-        used.classList.add('List');
-        spicelists.appendChild(used);
-
-        const amounts = create('div');
-        amounts.classList.add('Amounts');
-        spicelists.appendChild(amounts);
-
-        const unused = create('div');
-        unused.classList.add('List');
-        spicelists.appendChild(unused);
+    recipe ??= new Recipe;
 
 
+    const label = create('input');
+    label.placeholder = 'Recipe Name';
+    label.type = 'text';
+    label.value = recipe.name;
+    label.addEventListener('input',onEditName);
+    editor.appendChild(label);
 
-        const spices = Spices.spices();
+    const spicelists = create('div');
+    spicelists.classList.add('Spices');
+    editor.appendChild(spicelists);
+
+    const used_label = create('div');
+    used_label.textContent = 'Used';
+    used_label.classList.add('Label');
+    used_label.title = 'Spices used in your recipe.';
+    spicelists.appendChild(used_label);
+
+    spicelists.appendChild(create('div'));
+
+    const unused_label = create('div');
+    unused_label.textContent = 'Available';
+    unused_label.classList.add('Label');
+    unused_label.title = 'Spices you can add to your recipe.';
+    spicelists.appendChild(unused_label);
+
+    const used = create('div');
+    used.classList.add('List');
+    spicelists.appendChild(used);
+
+    const amounts = create('div');
+    amounts.classList.add('Amounts');
+    spicelists.appendChild(amounts);
+
+    const unused = create('div');
+    unused.classList.add('List');
+    spicelists.appendChild(unused);
 
 
-        for(let s = 0;s <= 16;s++)
-            addSpiceChoice(s);
 
-        recipe.spices.forEach(spiceBox);
-
-        updateAmount();
+    const spices = Spices.spices();
 
 
-        function updateAmount(){
+    for(let s = 0;s <= 16;s++)
+        addSpiceChoice(s);
 
-            [...amounts.children].forEach((input,index) => {
+    recipe.spices.forEach(spiceBox);
 
-                const spice = [...used.children][index]
+    updateAmount();
 
-                log('amount',input,index,spice)
 
-                if(!spice){
-                    input.oninput = () => {};
-                    return;
-                }
+    function updateAmount(){
+
+        [...amounts.children].forEach((input,index) => {
+
+            const spice = [...used.children][index];
+
+            let
+                onInput = ignore,
+                value = '';
+
+
+            if(spice){
 
                 const name = spice.textContent;
+                value = recipe.spices.get(name);
 
-                input.value = recipe.spices.get(name);
-
-                input.oninput = () => {
-
-                    const { dataset , value } = input;
-
-                    let amount = value
-                        .replace(/[^0-9]/g,'')
-                        .replace(/^0/,'');
-
-
-                    amount ??= '1';
-                    amount = parseInt(amount);
-
-                    if(amount < 1)
-                        amount = 1;
-
-                    if(amount > 255)
-                        amount = 255;
-
-                    input.value = amount;
-
-                    recipe.modifySpice(name,amount);
-                }
-            });
-        }
-
-
-        function addSpiceChoice(index){
-
-            let spice = spices[index] ?? '';
-            spice = spice.trim();
-
-            const usable = spice.length > 1;
-
-            const color = usable
-                ? stringColor(spice,20,50)
-                : '#9d9d9d' ;
-
-            const box = create('div');
-            box.textContent = spice;
-            box.style.backgroundColor = color
-            unused.appendChild(box);
-
-            if(usable){
-                box.classList.add('button');
-                box.addEventListener('click',onSpiceAdd);
+                onInput = checkInput;
             }
 
-            if(!usable || recipe.hasSpice(spice))
-                box.classList.add('Empty')
-
-            const input = create('input');
-            input.value = [...recipe.spices.values()][index];
-            input.type = 'text';
-            amounts.appendChild(input);
-
-            if(index >= recipe.spices.size)
-                input.classList.add('Empty');
+            input.value = value;
+            input.oninput = onInput;
+        });
+    }
 
 
+    function checkInput(){
+
+        const { dataset , value } = input;
+
+        let amount = value
+            .replace(/[^0-9]/g,'')
+            .replace(/^0/,'');
 
 
-            function onSpiceAdd(){
+        amount ??= '1';
+        amount = parseInt(amount);
 
-                if(box.classList.contains('Empty'))
-                    return;
+        if(amount < 1)
+            amount = 1;
 
-                recipe.addSpice(spice,1);
-                box.classList.add('Empty');
+        if(amount > 255)
+            amount = 255;
 
-                const i = [...amounts.children][recipe.spices.size - 1];
-                i.classList.remove('Empty');
-                i.value = 1;
+        input.value = amount;
 
-                spiceBox(1,spice);
+        recipe.modifySpice(name,amount);
+    }
 
-                updateAmount();
-            }
-        }
 
-        function onEditName(){
+    function addSpiceChoice(index){
 
-            let { value } = label;
+        let spice = spices[index] ?? '';
+        spice = spice.trim();
 
-            value = value
-                .replaceAll(/\s+/g,' ')
-                .substring(0,20);
+        const usable = spice.length > 1;
 
-            label.value = value;
+        const color = usable
+            ? stringColor(spice,20,50)
+            : '#9d9d9d' ;
 
-            value = value.trim();
+        const box = create('div');
+        box.textContent = spice;
+        box.style.backgroundColor = color
+        unused.appendChild(box);
 
-            if(value.length < 1)
-                value = null;
-
-            recipe.name = value;
-            // editor.style.backgroundColor = stringColor(recipe.name,20,60);
-        }
-
-        function spiceBox(amount,spice){
-
-            const box = create('div');
+        if(usable){
             box.classList.add('button');
-            box.textContent = spice;
-            box.style.backgroundColor = stringColor(spice,20,50);
-            box.addEventListener('click',onRemoveSpice);
-            used.appendChild(box);
+            box.addEventListener('click',onSpiceAdd);
+        }
 
-            function onRemoveSpice(){
+        if(!usable || recipe.hasSpice(spice))
+            box.classList.add('Empty')
 
-                box.remove();
-                recipe.removeSpice(spice);
+        const input = create('input');
+        input.value = [...recipe.spices.values()][index];
+        input.type = 'text';
+        amounts.appendChild(input);
 
-                const element = [...unused.children]
-                    .find((element) => element.textContent === spice);
+        if(index >= recipe.spices.size)
+            input.classList.add('Empty');
 
-                if(element){
-                    element.classList.remove('Empty');
-                    [...amounts.children][recipe.spices.size].classList.add('Empty');
-                }
 
-                updateAmount();
-            }
+
+
+        function onSpiceAdd(){
+
+            if(box.classList.contains('Empty'))
+                return;
+
+            recipe.addSpice(spice,1);
+            box.classList.add('Empty');
+
+            const i = [...amounts.children][recipe.spices.size - 1];
+            i.classList.remove('Empty');
+            i.value = 1;
+
+            spiceBox(1,spice);
+
+            updateAmount();
         }
     }
-})();
+
+    function onEditName(){
+
+        let { value } = label;
+
+        value = value
+            .replaceAll(/\s+/g,' ')
+            .substring(0,20);
+
+        label.value = value;
+
+        value = value.trim();
+
+        if(value.length < 1)
+            value = null;
+
+        recipe.name = value;
+    }
+
+    function spiceBox(amount,spice){
+
+        const box = create('div');
+        box.classList.add('button');
+        box.textContent = spice;
+        box.style.backgroundColor = stringColor(spice,20,50);
+        box.addEventListener('click',onRemoveSpice);
+        used.appendChild(box);
+
+        function onRemoveSpice(){
+
+            box.remove();
+            recipe.removeSpice(spice);
+
+            const element = [...unused.children]
+                .find((element) => element.textContent === spice);
+
+            if(element){
+                element.classList.remove('Empty');
+                [...amounts.children][recipe.spices.size].classList.add('Empty');
+            }
+
+            updateAmount();
+        }
+    }
+}
